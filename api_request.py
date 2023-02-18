161e631f98c79c4ca2ba7f29b858e782a69ba2f6
quilcummings <quil735@gmail.com>
2023-02-18 03:23:00 -0700

    get users on server? maybe

--- api_request.py
+++ api_request.py
@@ -1,13 +1,50 @@
 # This document sends a request to the API
 
-# import the library requests
-import requests
+username = 'nesh5910@colorado.edu'
+apikey = 'cxRa4yA7vHp63nU'
 
+# import the library requests
 from plexapi.myplex import MyPlexAccount
-account = MyPlexAccount('nesh5910@colorado.edu', 'cxRa4yA7vHp63nU')
+from plexapi.server import PlexServer
+from plexapi.media import TranscodeSession
+account = MyPlexAccount(username, apikey)
 plex = account.resource('T9 Plex').connect()  # returns a PlexServer instance
 
 # Example 1: List all unwatched movies.
-movies = plex.library.section('Movies')
-for video in movies.search(unwatched=True):
-    print(video.title)
+# movies = plex.library.section('Movies')
+# for video in movies.search(unwatched=True):
+#     print(video.title)
+
+media = plex.library.all()
+for thing in media:
+    print(thing.title)
+
+# for playlist in plex.playlists():
+#     print(playlist.title)
+
+# print(plex.playlists()[0].title)
+
+# get playlist
+playlists = plex.playlists()
+playlist = playlists[0]
+
+# get users on server
+
+def devices(self):
+        """ Returns a list of all :class:`~plexapi.myplex.MyPlexDevice` objects connected to the server. """
+        data = self.query(plex.key)
+        return [plex(self, elem) for elem in data]
+
+
+#device = plex.MyPlexDevice.id
+
+# get time stamp from main user on playlist
+
+# use media.Session and media.TranscodeSession
+# streamers share rating key UUID assigned to content
+# timestamp object
+
+print(plex._session)
+
+# whenever things aren't working, reload the page, open the networks tab in dev tools, and then search what pops up
+# need to get to "client" object and find "timeline" object - that has the best info
